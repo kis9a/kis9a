aa9a5843897dc66f82267c8c939975c998521e7e
kis9a <kis9ax@gmail.com>
2021-01-13 18:57:37 +0900

    init: image and shellscript

---
+++ kis9a.sh
@@ -0,0 +1,13 @@
+echo '
+Hello!!  
+Kaito.M*kis9a  
+Software Engineer (beginner),  
+Work in the terminal. Learning Typescript, Go, and Vim.  
+
+Infomation\
+
+https://nav.kis9a.com  
+zenn: https://zenn.dev/kis9a  
+notes: htttps://twitter.com/kis9a  
+twitter: https://twitter.com/kis9a  
+';
