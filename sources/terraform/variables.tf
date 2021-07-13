77a1d84e15ffc4f05b127a8e63086993fe93f0d8
kis9a <kis9ax@gmail.com>
2021-07-13 17:29:03 +0900

    sources: update 2021-07-13 terraform backend initialized

---
+++ sources/terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {
+  type    = string
+  default = "ap-northeast-1"
+}
+
+variable "profile" {
+  type    = string
+  default = "kis9a-src"
+}
+
+variable "hosted_zone" {
+  type    = string
+  default = "kis9a.com"
+}
+
+variable "domain_name" {
+  type    = string
+  default = "me.kis9a.com"
+}
