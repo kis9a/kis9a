ee32c47997bed2566992282022ec35b9256ece91
kis9a <kis9ax@gmail.com>
2021-07-16 01:28:44 +0900

    sources: update 2021-07-16 fix terraform

--- sources/terraform/variables.tf
+++ sources/terraform/variables.tf
@@ -8,25 +8,11 @@ variable "profile" {
   default = "kis9a-src"
 }
 
-variable "hosted_zone" {
-  type    = string
-  default = "kis9a.com"
-}
-
 variable "domain_name" {
   type    = string
   default = "me.kis9a.com"
 }
 
-variable "site_domain" {
-  default = "me.kis9a.com"
-}
-
-variable "root_domain" {
-  default = "kis9a.com"
-}
-
 variable "bucket_name" {
   default = "kis9a-sources"
 }
-
