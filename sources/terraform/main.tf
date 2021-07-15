9136bf7ba1387e62000d358ceb3c6b05c1a531b2
kis9a <kis9ax@gmail.com>
2021-07-16 01:28:44 +0900

    sources: update 2021-07-16 fix terraform

--- sources/terraform/main.tf
+++ sources/terraform/main.tf
@@ -12,41 +12,24 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-resource "aws_route53_zone" "site_zone" {
-  name = "kis9a.com"
-}
+# resource "aws_route53_zone" "site_zone" {
+#   name = "me.kis9a.com"
+# }
 
-module "acm" {
-  source      = "./modules/acm"
-  root_domain = "kis9a.com"
-  zone_id     = aws_route53_zone.site_zone.zone_id
-}
+# module "acm" {
+#   source      = "./modules/acm"
+#   root_domain = "kis9a.com"
+#   zone_id     = "Z09579232JIYGMEX3JQS5"
+# }
 
 module "static_site_root" {
   source              = "./modules/static_site"
-  bucket_name         = "kis9a-sources"
-  domain              = "kis9a.com"
-  zone_id             = aws_route53_zone.site_zone.zone_id
-  acm_certificate_arn = module.acm.certificate_arn
+  bucket_name         = var.bucket_name
+  domain              = var.domain_name
+  zone_id             = "Z09579232JIYGMEX3JQS5"
+  acm_certificate_arn = "	arn:aws:acm:us-east-1:298276046670:certificate/55c5ce44-b780-45ea-b5b2-c550cdcde710"
 }
 
-module "static_site_foo" {
-  source              = "./modules/static_site"
-  bucket_name         = "kis9a-sources"
-  domain              = "me.kis9a.com"
-  zone_id             = aws_route53_zone.site_zone.zone_id
-  acm_certificate_arn = module.acm.certificate_arn
-}
-
-module "static_site_bar" {
-  source              = "./modules/static_site"
-  bucket_name         = "kis9a-sources"
-  domain              = "me.kis9a.com"
-  zone_id             = aws_route53_zone.site_zone.zone_id
-  acm_certificate_arn = module.acm.certificate_arn
-}
-
-
-output "name_servers" {
-  value = aws_route53_zone.site_zone.name_servers
-}
+# output "name_servers" {
+#   value = aws_route53_zone.site_zone.name_servers
+# }
