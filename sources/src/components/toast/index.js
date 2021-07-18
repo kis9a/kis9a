12682423acb73e1f9c956cd28d4526f0b8a07f17
kis9a <kis9ax@gmail.com>
2021-07-18 23:44:17 +0900

    sources: update 2021-07-18 javascript add toast function fadeIn fadeOut

--- sources/src/components/toast/index.js
+++ sources/src/components/toast/index.js
@@ -22,10 +22,42 @@ export const Toast = (
   toast.style.fontSize = "1.6rem";
   toast.style.borderRadius = "1rem 0.5rem 0rem 0.5rem";
   toast.style.border = "2px solid #e6e6e6";
+  fadeIn(toast, 500);
+  setTimeout(() => {
+    fadeOut(toast, 500);
+    setTimeout(() => {
+      document.body.removeChild(toast);
+      toast.style.display = "none";
+    }, 500);
+  }, opts.time);
   toast.appendChild(document.createTextNode(msg));
   document.body.appendChild(toast);
-  setTimeout(function () {
-    toast.style.display = "none";
-    document.body.removeChild(toast);
-  }, opts.time);
 };
+
+function fadeIn(el, time) {
+  el.style.opacity = 0;
+  var last = +new Date();
+  var tick = function () {
+    el.style.opacity = +el.style.opacity + (new Date() - last) / time;
+    last = +new Date();
+    if (+el.style.opacity < 1) {
+      (window.requestAnimationFrame && requestAnimationFrame(tick)) ||
+        setTimeout(tick, 16);
+    }
+  };
+  tick();
+}
+
+function fadeOut(el, time) {
+  el.style.opacity = 1;
+  var last = +new Date();
+  var tick = function () {
+    el.style.opacity = +el.style.opacity - (new Date() - last) / time;
+    last = +new Date();
+    if (+el.style.opacity > 0) {
+      (window.requestAnimationFrame && requestAnimationFrame(tick)) ||
+        setTimeout(tick, 16);
+    }
+  };
+  tick();
+}
