23551b8248c0c5586463d3fdc280f82bdfc5e9cf
kis9a <kis9ax@gmail.com>
2021-07-18 20:15:24 +0900

    sources: update 2021-07-18 sytle: header responsive design changed

--- sources/src/components/header/index.js
+++ sources/src/components/header/index.js
@@ -1,7 +1,6 @@
 import { h, text } from "/modules/js/hyperapp.js";
 import { routes } from "/modules/js/router.js";
 import { LinkIcon } from "/components/link";
-import { svg_dot_horizon } from "/components/icons";
 import "./index.css";
 
 var path = require("./path.js");
@@ -41,16 +40,7 @@ export const Header = () => {
         ]),
         h(
           "div",
-          {
-            class: "menu-icon link-icon",
-            innerHTML: svg_dot_horizon,
-            onclick: toggleViewLinks,
-          },
-          text("menu")
-        ),
-        h(
-          "div",
-          { class: `links ${window.innerWidth < 600 ? "none" : ""}` },
+          { class: "links" },
           routes.map((r) => LinkIcon(r.name, { active: r.name !== c.name }))
         ),
       ]),
@@ -60,11 +50,5 @@ export const Header = () => {
 
 const toNavigation = (state) => {
   window.open("https://nav.kis9a.com", "_blank");
-  return { ...state };
-};
-
-const toggleViewLinks = (state) => {
-  const links = document.getElementsByClassName("links");
-  links[0].classList.toggle("none");
   return { ...state };
 };
